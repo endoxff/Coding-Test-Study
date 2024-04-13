426f5442f2480824f484e8279132057e7a4de80b
mk-star <ssunn0812@naver.com>
2024-04-13 23:38:15 +0900

    2주차 문제 풀이

---
+++ week02/BOJ_2075_N번째_큰_수/김민경.java
@@ -0,0 +1,32 @@
+package week02.BOJ_2075_N번째_큰_수;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.StringTokenizer;
+
+public class 김민경 {
+    public static void main(String[] args) throws Exception {
+
+        PriorityQueue pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+
+        String[] strings1 = new String[N];
+        String[] strings2 = new String[N];
+        StringTokenizer st; 
+        for(int i = 0; i < N; i++) {
+            strings1 = br.readLine().split(" ");
+            for(int j = 0; j < N; j++) {
+                pq.offer(Integer.parseInt(strings1[j]));
+            }
+        }
+        for(int i = 0; i < N - 1; i++) {
+            pq.remove();
+        }
+        System.out.println(pq.peek());
+    }
+}
