24d9a5387e4127e3718ff4c9d44a6540c765bb3c
mk-star <ssunn0812@naver.com>
2024-04-19 20:52:42 +0900

    BOJ_2606_바이러스 / S3 / O

---
+++ week03/BOJ_2606_바이러스/김민경.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Main {
+    static int[][] adj;
+    static boolean[] visited;
+    static int count = 0;
+
+    static void dfs(int vc, int v) {
+        visited[vc] = true;
+        count++;
+        for(int i = 1; i <= v; i++) {
+            if(adj[vc][i] == 1 && visited[i] == false) {
+                dfs(i, v);
+            }
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int v = Integer.parseInt(br.readLine());
+        int e = Integer.parseInt(br.readLine());
+        adj = new int[v + 1][v + 1];
+        visited = new boolean[v + 1];
+    
+        for(int i = 1; i <= v; i++) {
+            visited[i] = false;
+            for(int j = 1; j <= v; j++) {
+                adj[i][j] = 0;
+            }
+        }
+
+        for(int i = 0; i < e; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int c1 = Integer.parseInt(st.nextToken());
+            int c2 = Integer.parseInt(st.nextToken());
+            adj[c1][c2] = adj[c2][c1] = 1;
+        }
+        dfs(1, v);
+        System.out.println(count - 1);
+    }
+}
