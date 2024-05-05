b47a0217c5fd53be76c839fbcbc01b33b3d9259a
강지륜 <kjr2699@naver.com>
2024-05-05 15:22:40 +0900

    BOJ_7576_토마토 / G5 / O

--- week04/BOJ_7576_토마토/강지륜.java
+++ week04/BOJ_7576_토마토/강지륜.java
@@ -11,7 +11,6 @@ public class Main {
 	static int[] dx = { -1, 0, 1, 0 };
 	static int[] dy = { 0, 1, 0, -1 };	
 
-	static ArrayList<int[]> list = new ArrayList<int[]>();
 	static Queue<int[]> queue = new LinkedList<>();
 	
 	public static void main(String[] args) throws IOException {
