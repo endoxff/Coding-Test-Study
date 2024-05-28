71ad433987dc1b5064c4f5ccf0b2e5319519483d
mk-star <ssunn0812@naver.com>
2024-05-28 20:28:44 +0900

    BOJ_2156_포도주_시식 / S1 / O

--- week07/BOJ_2156_포도주_시식/김민경.java
+++ week07/BOJ_2156_포도주_시식/김민경.java
@@ -22,7 +22,6 @@ public class Main {
             dp[2] = arr[1] + arr[2];
             for(int i = 3; i < n + 1; i++) {
                 dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1]+ arr[i]));
-                dp[i] = Math.max(dp[i - 1], dp[i]);
             }
             System.out.println(dp[n]);
         }
